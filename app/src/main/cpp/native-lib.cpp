9822d3fae95a2f064845749387dc5cd687cad60d
wanghaihui <465495722@qq.com>
2019-03-27 17:46:24 +0800

    crash situation

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,11 +2,22 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring
-
 JNICALL
 Java_com_wc_conquer_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+// 引起crash
+void crash() {
+    volatile int *a = (int *) (NULL);
+    *a = 1;
+}
+
+extern "C" JNIEXPORT void
+JNICALL
+Java_com_wc_conquer_MainActivity_crash(JNIEnv *env, jobject obj) {
+    crash();
 }
--- app/src/main/java/com/wc/conquer/MainActivity.kt
+++ app/src/main/java/com/wc/conquer/MainActivity.kt
@@ -1,18 +1,63 @@
 package com.wc.conquer
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import com.wc.breakpad.BreakpadInit
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private val writeExternalStorageRequestCode: Int = 100
+    private var externalReportPath: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 申请访问sdcard权限
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    writeExternalStorageRequestCode)
+        } else {
+            initExternalReportPath()
+        }
+
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
+
+        sample_text.setOnClickListener({
+            initBreakPad()
+            crash()
+        })
     }
+
+    private fun initExternalReportPath() {
+        externalReportPath = File(Environment.getExternalStorageDirectory(), "crashDump")
+        if (!externalReportPath!!.exists()) {
+            externalReportPath!!.mkdirs();
+        }
+    }
+
+    private fun initBreakPad() {
+        if (externalReportPath == null) {
+            externalReportPath = File(filesDir, "crashDump")
+            if (!externalReportPath!!.exists()) {
+                externalReportPath!!.mkdirs()
+            }
+        }
+        BreakpadInit.initBreakpad(externalReportPath!!.absolutePath)
+    }
+
+    private external fun crash()
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
@@ -26,5 +71,11 @@ class MainActivity : AppCompatActivity() {
         init {
             System.loadLibrary("native-lib")
         }
+    }
+
+    // 观看写法
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        initExternalReportPath()
     }
 }
