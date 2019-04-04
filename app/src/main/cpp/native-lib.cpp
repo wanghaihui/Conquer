f0985f93222813b0707f852df2733ccba6ca45c7
wanghaihui <465495722@qq.com>
2019-04-04 12:00:49 +0800

    c pointer

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,15 @@
 #include <jni.h>
 #include <string>
+#include <stdio.h>
+#include <android/log.h>
+
+#define LOG_TAG "haihui"
+
+#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 extern "C" JNIEXPORT jstring
 JNICALL
@@ -16,8 +26,17 @@ void crash() {
     *a = 1;
 }
 
+void pointer() {
+    int a = 100;
+    char str[20] = "c.biancheng.net";
+    ALOGD("%#X, %#X\n", (unsigned int) &a, (unsigned int) str);
+}
+
 extern "C" JNIEXPORT void
 JNICALL
 Java_com_wc_conquer_MainActivity_crash(JNIEnv *env, jobject obj) {
-    crash();
+    //crash();
+    pointer();
 }
+
+
--- app/src/main/java/com/wc/conquer/MainActivity.kt
+++ app/src/main/java/com/wc/conquer/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
     private fun initExternalReportPath() {
         externalReportPath = File(Environment.getExternalStorageDirectory(), "crashDump")
         if (!externalReportPath!!.exists()) {
-            externalReportPath!!.mkdirs();
+            externalReportPath!!.mkdirs()
         }
     }
 
